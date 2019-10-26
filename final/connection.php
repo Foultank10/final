07d38f0500ef97e5859d0ba782a57c8d25a56bb2
Foultank10 <53558921+Foultank10@users.noreply.github.com>
2019-10-25 21:17:47 -0700

    Add files via upload

---
+++ final/connection.php
@@ -0,0 +1,12 @@
+  
+<?php
+function Connect()
+{
+	$dbhost = "localhost";
+	$dbuser = "root";
+	$dbpass = "";
+	$dbname = "myfoodstop";
+	$conn = new mysqli($dbhost, $dbuser, $dbpass, $dbname) or die($conn->connect_error);
+	return $conn;
+}
+?>
---
+++ final/paytm.php
@@ -0,0 +1,247 @@
+<?php
+session_start();
+require 'connection.php';
+$conn = Connect();
+if(!isset($_SESSION['login_user2']) || !isset($_SESSION['cart'])){
+header("location: customerlogin.php"); //Redirecting to myrestaurant Page
+}
+
+?>
+
+<html>
+
+  <head>
+    <title> Online Payment | MyFoodStop </title>
+  </head>
+
+  <link rel="stylesheet" type = "text/css" href ="css/COD.css">
+  <link rel="stylesheet" type = "text/css" href ="css/bootstrap.min.css">
+  <script type="text/javascript" src="js/jquery.min.js"></script>
+  <script type="text/javascript" src="js/bootstrap.min.js"></script>
+
+  <body>
+
+  <!--Back to top button..................................................................................-->
+    <button onclick="topFunction()" id="myBtn" title="Go to top">
+      <span class="glyphicon glyphicon-chevron-up"></span>
+    </button>
+  <!--Javacript for back to top button....................................................................-->
+    <script type="text/javascript">
+      window.onscroll = function()
+      {
+        scrollFunction()
+      };
+
+      function scrollFunction(){
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+          document.getElementById("myBtn").style.display = "block";
+        } else {
+          document.getElementById("myBtn").style.display = "none";
+        }
+      }
+
+      function topFunction() {
+        document.body.scrollTop = 0;
+        document.documentElement.scrollTop = 0;
+      }
+    </script>
+
+    <nav class="navbar navbar-inverse navbar-fixed-top navigation-clean-search" role="navigation">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#myNavbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="index.php">MyFoodStop</a>
+        </div>
+
+        <div class="collapse navbar-collapse " id="myNavbar">
+          <ul class="nav navbar-nav">
+            <li><a href="index.php">Home</a></li>
+            <li><a href="aboutus.php">About</a></li>
+            <li><a href="contactus.php">Contact Us</a></li>
+
+          </ul>
+
+<?php
+if(isset($_SESSION['login_user1'])){
+
+?>
+
+
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="#"><span class="glyphicon glyphicon-user"></span> Welcome <?php echo $_SESSION['login_user1']; ?> </a></li>
+            <li><a href="myrestaurant.php">MANAGER CONTROL PANEL</a></li>
+            <li><a href="logout_m.php"><span class="glyphicon glyphicon-log-out"></span> Log Out </a></li>
+          </ul>
+          
+<?php
+}
+else if (isset($_SESSION['login_user2'])) {
+  ?>
+           <ul class="nav navbar-nav navbar-right">
+            <li><a href="#"><span class="glyphicon glyphicon-user"></span> Welcome <?php echo $_SESSION['login_user2']; ?> </a></li>
+            <li><a href="foodlist.php"><span class="glyphicon glyphicon-cutlery"></span> Food Zone </a></li>
+            <li><a href="cart.php"><span class="glyphicon glyphicon-shopping-cart"></span> Cart
+             (<?php
+              if(isset($_SESSION["cart"])){
+              $count = count($_SESSION["cart"]); 
+              echo "$count"; 
+            }
+              else
+                echo "0";
+              ?>)
+              </a></li>
+            <li><a href="logout_u.php"><span class="glyphicon glyphicon-log-out"></span> Log Out </a></li>
+          </ul>
+  <?php        
+}
+else {
+
+  ?>
+
+<ul class="nav navbar-nav navbar-right">
+            <li><a href="#" class="dropdown-toggle active" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><span class="glyphicon glyphicon-user"></span> Sign Up <span class="caret"></span> </a>
+                <ul class="dropdown-menu">
+              <li> <a href="customersignup.php"> User Sign-up</a></li>
+              <li> <a href="managersignup.php"> Manager Sign-up</a></li>
+              <li> <a href="#"> Admin Sign-up</a></li>
+            </ul>
+            </li>
+
+            <li><a href="#" class="dropdown-toggle active" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><span class="glyphicon glyphicon-log-in"></span> Login <span class="caret"></span></a>
+              <ul class="dropdown-menu">
+              <li> <a href="customerlogin.php"> User Login</a></li>
+              <li> <a href="managerlogin.php"> Manager Login</a></li>
+              <li> <a href="#"> Admin Login</a></li>
+            </ul>
+            </li>
+          </ul>
+
+<?php
+}
+?>
+
+
+        </div>
+
+      </div>
+    </nav>
+    <?php
+$gtotal = 0;
+  foreach($_SESSION["cart"] as $keys => $values)
+  {
+
+    $F_ID = $values["food_id"];
+    $foodname = $values["food_name"];
+    $quantity = $values["food_quantity"];
+    $price =  $values["food_price"];
+    $total = ($values["food_quantity"] * $values["food_price"]);
+    $R_ID = $values["R_ID"];
+    $username = $_SESSION["login_user2"];
+    $order_date = date('Y-m-d');
+    
+    $gtotal = $gtotal + $total;
+
+
+     $query = "INSERT INTO ORDERS (F_ID, foodname, price,  quantity, order_date, username, R_ID) 
+              VALUES ('" . $F_ID . "','" . $foodname . "','" . $price . "','" . $quantity . "','" . $order_date . "','" . $username . "','" . $R_ID . "')";
+             
+
+              $success = $conn->query($query);         
+
+      if(!$success)
+      {
+        ?>
+        <div class="container">
+          <div class="jumbotron">
+            <h1>Something went wrong!</h1>
+            <p>Try again later.</p>
+          </div>
+        </div>
+
+        <?php
+      }
+      
+  }
+
+        ?>
+
+
+
+<div class="container">
+    <div class="row">
+        <div class="jumbotron">
+          <h1 class="text-center">Online Payment</h1>
+          <p class="text-center">Enter your payment details below.</p>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-6 col-md-offset-3">
+        <form method="post" action="paytm/PaytmKit/pgRedirect.php">
+		<table border="1">
+			<tbody>
+				<tr>
+					<th>S.No</th>
+					<th>Label</th>
+					<th>Value</th>
+				</tr>
+				<tr>
+					<td>1</td>
+					<td><label>ORDER_ID::*</label></td>
+					<td><input id="ORDER_ID" tabindex="1" maxlength="20" size="20"
+						name="ORDER_ID" autocomplete="off"
+						value="<?php echo  "ORDS" . rand(10000,99999999)?>">
+					</td>
+				</tr>
+				<tr>
+					<td>2</td>
+					<td><label>CUSTID ::*</label></td>
+					<td><input id="CUST_ID" tabindex="2" maxlength="12" size="12" name="CUST_ID" autocomplete="off" value="CUST001"></td>
+				</tr>
+				<tr>
+					<td>3</td>
+					<td><label>INDUSTRY_TYPE_ID ::*</label></td>
+					<td><input id="INDUSTRY_TYPE_ID" tabindex="4" maxlength="12" size="12" name="INDUSTRY_TYPE_ID" autocomplete="off" value="Retail"></td>
+				</tr>
+				<tr>
+					<td>4</td>
+					<td><label>Channel ::*</label></td>
+					<td><input id="CHANNEL_ID" tabindex="4" maxlength="12"
+						size="12" name="CHANNEL_ID" autocomplete="off" value="WEB">
+					</td>
+				</tr>
+				<tr>
+					<td>5</td>
+					<td><label>txnAmount*</label></td>
+					<td><input title="TXN_AMOUNT" tabindex="10"
+						type="text" name="TXN_AMOUNT"
+						value="<?php echo "$gtotal"; ?>">
+					</td>
+				</tr>
+				<tr>
+					<td></td>
+					<td></td>
+					<td><input value="CheckOut" type="submit"	onclick=""></td>
+				</tr>
+			</tbody>
+		</table>
+		* - Mandatory Fields
+	</form>
+            
+        </div>
+    </div>
+</div>
+
+
+           </body>
+
+  <footer class="container-fluid bg-4 text-center">
+  <br>
+  <p> MyFoodStop 2019 | &copy All Rights Reserved </p>
+  <br>
+  </footer>
+</html>
